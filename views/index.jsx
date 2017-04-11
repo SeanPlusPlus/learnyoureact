d06632dd6f589fffc94512d93ddd6b2d6f1b80cb
Sean Stephenson <sms@pixar.com>
2017-04-11 11:09:00 -0700

    add styles

--- views/index.jsx
+++ views/index.jsx
@@ -43,17 +43,17 @@ class Todo extends React.Component {
   render() {
     return (
       <tr>
-        <td style={{border: "1px solid black"}}>
+        <td style={style.tableContent}>
           <input
             type="checkbox"
             checked={this.state.checked}
             onChange={this.handleChange}
           />
         </td>
-        <td style={{border: "1px solid black"}}>
+        <td style={style.tableContent}>
           {this.props.title}
         </td>
-        <td style={{border: "1px solid black"}}>
+        <td style={style.tableContent}>
           {this.props.children}
         </td>
       </tr>
@@ -72,5 +72,11 @@ class TodoForm extends React.Component {
         I am a TodoForm.
       </div>
     )
+  }
+}
+
+let style = {
+  tableContent: {
+    border: "1px solid black"
   }
 }
